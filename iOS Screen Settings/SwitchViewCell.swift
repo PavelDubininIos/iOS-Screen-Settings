1e118fc4be31b155043edc54f7523345475e5383
Pavel Dubinin <pavel.ois.develop@gmail.com>
2022-01-07 21:24:25 +0300

    Config view of switch with cell.
    Config view of switch with cell.

---
+++ iOS Screen Settings/SwitchViewCell.swift
@@ -0,0 +1,76 @@
+
+import UIKit
+
+class SwitchViewCell: UITableViewCell {
+    
+    static let indentifier = "SwitchViewCell"
+    
+    private let iconContainer: UIView = {
+        let view = UIView()
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 8
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    private let iconImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.tintColor = .white
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let title: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private let mySwitch: UISwitch = {
+        let mySwitch =  UISwitch()
+        mySwitch.onTintColor = .systemGreen
+        return mySwitch
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupHierarchy()
+        contentView.clipsToBounds = true
+        accessoryType = .none
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let size: CGFloat = contentView.frame.size.height - 12
+        iconContainer.frame = CGRect(x: 15, y: 6, width: size, height: size)
+        
+        mySwitch.frame = CGRect(x: (contentView.frame.size.width - 20 - mySwitch.frame.size.width),
+                                y: (contentView.frame.size.height - mySwitch.frame.size.height)/2,
+                                width: mySwitch.frame.size.width,
+                                height: mySwitch.frame.size.height)
+        mySwitch.sizeToFit()
+        
+        let imageSize: CGFloat = size/1.5
+        iconImage.frame = CGRect(x: (size - imageSize)/2,
+                                     y: (size - imageSize)/2,
+                                     width: imageSize,
+                                     height: imageSize)
+        
+        title.frame = CGRect(x: 25 + iconContainer.frame.size.width,
+                             y: 0,
+                             width: contentView.frame.size.width - 20 - iconContainer.frame.size.width,
+                             height: contentView.frame.size.height)
+    }
+    
+    func setupHierarchy() {
+        contentView.addSubview(title)
+        contentView.addSubview(iconContainer)
+        contentView.addSubview(mySwitch)
+        iconContainer.addSubview(iconImage)
+    }
+}
+
