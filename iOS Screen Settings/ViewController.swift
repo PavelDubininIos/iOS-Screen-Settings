b3fa042a177ac3bc9e218eb4d397f8350ce059d7
Pavel Dubinin <pavel.ois.develop@gmail.com>
2022-01-07 21:08:31 +0300

    Initial Commit

---
+++ iOS Screen Settings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Screen Settings
+//
+//  Created by Павел Дубинин on 07.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
